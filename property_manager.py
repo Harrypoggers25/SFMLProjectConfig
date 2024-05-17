ac2a8daf29415586a50514515b0503fb2556a736
Harry Poggers <mfa.g4e@gmail.com>
2024-05-17 10:44:54 +0800

    Linker Textbox update
    -linkers are now saved in arrays

--- property_manager.py
+++ property_manager.py
@@ -1,39 +1,49 @@
 import os
-from PyQt5.QtWidgets import QFileDialog, QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QLineEdit, QTextEdit
 
 class PropertyManager:
-    @staticmethod
-    def getFile(window: QMainWindow):
+    def __init__(self):
+        self.linkers = ['sfml-graphics.lib', 'sfml-system.lib', 'sfml-window.lib', 'sfml-network.lib', 'sfml-audio.lib']
+        self.dlinkers = ['sfml-graphics-d.lib', 'sfml-system-d.lib', 'sfml-window-d.lib', 'sfml-network-d.lib', 'sfml-audio-d.lib']
+        self.project_name = None
+
+    def setLinkers(self, textbox: QTextEdit):
+        self.linkers = [line.strip() for line in textbox.toPlainText().split('\n') if line.strip()]
+    
+    def setDLinkers(self, textbox: QTextEdit):
+        self.dlinkers = [line.strip() for line in textbox.toPlainText().split('\n') if line.strip()]
+
+    def getFile(self, window: QMainWindow):
         filepath, _ = QFileDialog.getOpenFileName(window, 'Select Solution File', '', 'Solution File (*.sln)')
         if filepath:
             filename = os.path.basename(filepath)[:-len('.sln')]
+            self.project_name = filename
             filedir = filepath[:-len('/' + filename + '.sln')]
             return {'name': filename, 'path': filepath, 'dir': filedir}
         return None
     
-    @staticmethod
-    def getPath(window: QMainWindow, initial_dir = None):
+    def getPath(self, window: QMainWindow, initial_dir = None):
         folderPath = QFileDialog.getExistingDirectory(window, 'Select Folder', initial_dir) if initial_dir else QFileDialog.getExistingDirectory(window, 'Select Folder')
         if folderPath:
             return folderPath
         return None
     
-    @staticmethod
-    def updateSoltDir(window: QMainWindow, textbox: QLineEdit):
-        file = PropertyManager.getFile(window)
+    def updateSoltDir(self, window: QMainWindow, textbox: QLineEdit):
+        file = self.getFile(window)
         if file:
             textbox.setText(file['dir'])
     
-    @staticmethod
-    def updatePath(window: QMainWindow, textbox: QLineEdit):
-        folder = PropertyManager.getPath(window, textbox.text())
+    def updatePath(self, window: QMainWindow, textbox: QLineEdit):
+        folder = self.getPath(window, textbox.text())
         if folder:
             textbox.setText(folder)
 
-    @staticmethod
-    def getRelPath(pathName: str):
-        return (os.path.join(os.path.dirname(os.path.abspath(__file__)), pathName)).replace('\\', '/')
+    def getRelPath(self, pathName: str):
+        path = (os.path.join(os.path.dirname(os.path.abspath(__file__)), pathName)).replace('\\', '/')
+        return path[:path.find(':')].upper() + path[path.find(':'):]
+         
+    def getLinkers(self):
+        return '\n'.join(self.linkers) + '\n'
     
-    @staticmethod
-    def getLinkers():
-        return 'sfml-graphics.lib\nsfml-system.lib\nsfml-window.lib\nsfml-network.lib\nsfml-audio.lib\n'
+    def getDLinkers(self):
+        return '\n'.join(self.dlinkers) + '\n'
